146a6422e3565f692212afe10bcd0c96b1f45170
Ricardo Santos <ricardosantos130100@gmail.com>
2026-04-07 22:33:24 -0300

    Upload files with code

--- README.md
+++ README.md
@@ -3,15 +3,9 @@
 O principal objetivo do S3 é armazenar arquivos de texto, áudio, vídeo ou imagem. Este armazenamento em muitos casos pode ser utilizado de maneira inteligente, conversando com outros serviços em funcionamento na AWS, como uma função Lambda ou EC2.
 Foi utilizado em conjunto ao S3 o AWS CloudFront, ele tem por objeto acelerar a entrega de conteúdo a clientes globais, busca reduzir ao máximo a latência e para isso faz uso das Edge locations, por estarem mais próximos ao usuários.
 
-**Stack / Tecnologias** — HTML e CSS, AWS S3 e CLOUDFRONT
+**Stack / Tecnologias —** HTML e CSS, AWS S3 e CLOUDFRONT
 
-
-**Arquitetura —**
-
-
-**Como fazer deploy —**
-
-**URL:**
+**Como fazer deploy —** Primeiramente é preciso desativar o bloqueio de acesso público indo em permissões dentro da _bucket_. Ainda na aba permissões, é necessário criar uma política para a _bucket_ autorizando a leitura e a obtenção do(s) arquivo(s). Por último, basta ir em propriedades e configurar a _Hospedagem de site estático_.
 
 
 ![static_site_s3](https://github.com/user-attachments/assets/b3e942fc-8edf-4766-8afc-7b8375138144)
--- src/createS3.py
+++ src/createS3.py
@@ -1,6 +1,16 @@
 import logging
+import json
+import os
+import mimetypes
 import boto3
 from botocore.exceptions import ClientError
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
+
 
 class AWS:
 
@@ -14,25 +24,67 @@ class AWS:
         try:
             new_bucket = self.create_name_bucket()
             self.s3_client.create_bucket(Bucket=new_bucket["name_bucket"])
-            print(f"S3 bucket {new_bucket["name_bucket"]} created successfully in region {new_bucket["region"]}")
+            logging.info(f"S3 bucket {new_bucket["name_bucket"]} created successfully in region {new_bucket["region"]}")
         except ClientError as e:
             logging.error(e)
             return False
-        return True
+        return new_bucket["name_bucket"]
 
     def create_name_bucket(self):
         
         region_default = self.s3_client.meta.region_name
         response = self.sts_client.get_caller_identity()
         account_id = response["Account"]
-    
-        
         name_bucket = f"{self.initial_bucket_name}-{account_id}-{region_default}"
-        
         return {"name_bucket":name_bucket, "region": region_default}
-
+    
+    def createS3public(self):
+        name = aws.create_bucket()
+        self.s3_client.delete_public_access_block(Bucket= name)
+        logging.info("Bloqueio de acesso público removido com sucesso")
+        return name
+    
+    def createbucketpoliy(self):
+        bucket = self.createS3public()
+        arn = f"arn:aws:s3:::{bucket}/*"
+        policy = {
+                "Version": "2012-10-17",
+                    "Statement": [
+                        {
+                            "Sid": "PublicReadGetObject",
+                            "Effect": "Allow",
+                            "Principal": "*",
+                            "Action": "s3:GetObject",
+                            "Resource": arn
+                        }
+                    ]
+                }
+        policy_string = json.dumps(policy)
+        try:
+            self.s3_client.put_bucket_policy(
+                Bucket = bucket,
+                Policy = policy_string
+            )
+            logging.info("Política adicionada com sucesso")
+            return bucket
+        except Exception as e:
+            print("Erro ao adicionar a política", e)
+            
+    def configure_static_website(self):
+        bucket = self.createbucketpoliy()
+        self.s3_client.put_bucket_website(
+        Bucket=bucket,
+        WebsiteConfiguration={
+            'ErrorDocument': {'Key': 'error.html'},
+            'IndexDocument': {'Suffix': 'index.html'},
+        })
+        logging.info("Hospedagem de site estático habilitado")
+        
+    def upload_files(self, BUCKET_NAME: str):
+        logging.info("Enviando os arquivos")
+        files = ['index.html', 'error.html']
 
 if __name__ == "__main__":
 
     aws = AWS("static-site-bucket")
-    aws.create_bucket()
+    aws.configure_static_website()
