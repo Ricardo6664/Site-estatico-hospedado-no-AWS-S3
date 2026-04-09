c90d3a6590ef25245a25bf9f44e0c4b94ee3f552
Ricardo Santos <ricardosantos130100@gmail.com>
2026-04-08 21:44:14 -0300

    Creating bucket and add files with boto3

--- src/createS3.py
+++ src/createS3.py
@@ -1,14 +1,14 @@
 import logging
 import json
 import os
-import mimetypes
+import sys
 import boto3
 from botocore.exceptions import ClientError
 
 
 logging.basicConfig(
     level=logging.INFO,
-    format='%(asctime)s - %(levelname)s - %(message)s'
+    format='%(asctime)s | %(levelname)s | %(message)s'
 )
 
 
@@ -18,6 +18,7 @@ class AWS:
         self.initial_bucket_name = bucket_name
         self.s3_client = boto3.client("s3")
         self.sts_client = boto3.client("sts")
+        self.separate_message = "#############################"
 
 
     def create_bucket(self):
@@ -25,24 +26,32 @@ class AWS:
             new_bucket = self.create_name_bucket()
             self.s3_client.create_bucket(Bucket=new_bucket["name_bucket"])
             logging.info(f"S3 bucket {new_bucket["name_bucket"]} created successfully in region {new_bucket["region"]}")
+            self.upload_files(new_bucket["name_bucket"])
         except ClientError as e:
             logging.error(e)
             return False
         return new_bucket["name_bucket"]
 
     def create_name_bucket(self):
+        try:
+            region_default = self.s3_client.meta.region_name
+            response = self.sts_client.get_caller_identity()
+            account_id = response["Account"]
+            name_bucket = f"{self.initial_bucket_name}-{account_id}-{region_default}"
+            return {"name_bucket":name_bucket, "region": region_default}
         
-        region_default = self.s3_client.meta.region_name
-        response = self.sts_client.get_caller_identity()
-        account_id = response["Account"]
-        name_bucket = f"{self.initial_bucket_name}-{account_id}-{region_default}"
-        return {"name_bucket":name_bucket, "region": region_default}
-    
+        except Exception as e:
+            logging.error(e)
+            sys.exit(1)
+
     def createS3public(self):
-        name = aws.create_bucket()
-        self.s3_client.delete_public_access_block(Bucket= name)
-        logging.info("Bloqueio de acesso público removido com sucesso")
-        return name
+        try:
+            name = aws.create_bucket()
+            self.s3_client.delete_public_access_block(Bucket= name)
+            logging.info("Public access block remove successfully")
+            return name
+        except Exception as e:
+            logging.error(e)
     
     def createbucketpoliy(self):
         bucket = self.createS3public()
@@ -65,26 +74,37 @@ class AWS:
                 Bucket = bucket,
                 Policy = policy_string
             )
-            logging.info("Política adicionada com sucesso")
+            logging.info("Role added successfully")
             return bucket
         except Exception as e:
-            print("Erro ao adicionar a política", e)
+            logging.error("Error in add role", e)
             
     def configure_static_website(self):
-        bucket = self.createbucketpoliy()
-        self.s3_client.put_bucket_website(
-        Bucket=bucket,
-        WebsiteConfiguration={
-            'ErrorDocument': {'Key': 'error.html'},
-            'IndexDocument': {'Suffix': 'index.html'},
-        })
-        logging.info("Hospedagem de site estático habilitado")
-        
-    def upload_files(self, BUCKET_NAME: str):
-        logging.info("Enviando os arquivos")
-        files = ['index.html', 'error.html']
+        try:
+            bucket = self.createbucketpoliy()
+            self.s3_client.put_bucket_website(
+            Bucket=bucket,
+            WebsiteConfiguration={
+                'ErrorDocument': {'Key': 'error.html'},
+                'IndexDocument': {'Suffix': 'index.html'},
+            })
+            logging.info("Static Website habilited")
+        except Exception as e:
+            logging.error(e)
 
+    def upload_files(self, BUCKET_NAME: str):
+        try:
+            logging.info("Enviando os arquivos")
+            files = ['index.html', 'error.html']
+            for index, name in enumerate(files):
+                path_complete = os.path.join(os.getcwd(),files[index])
+                self.s3_client.upload_file(path_complete, BUCKET_NAME, name, ExtraArgs={'ContentType': 'text/html'})
+                logging.info(f"File {name} created successfully")
+        except Exception as e:
+            logging.error(e)
+            
 if __name__ == "__main__":
 
     aws = AWS("static-site-bucket")
     aws.configure_static_website()
+    
